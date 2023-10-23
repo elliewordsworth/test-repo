fdc864e694f3ba748064e909b8485551f0d36739
Ellie <91384683+elliewordsworth@users.noreply.github.com>
2023-10-23 17:29:46 +0100

    Add files via upload

---
+++ CryptoException.java
@@ -0,0 +1,12 @@
+/*
+ * Decompiled with CFR 0.150.
+ */
+public class CryptoException
+extends Exception {
+    public CryptoException() {
+    }
+
+    public CryptoException(String string, Throwable throwable) {
+        super(string, throwable);
+    }
+}
